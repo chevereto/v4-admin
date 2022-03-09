a8ffda512b27a152dce8ee4da45e5903b12ec711
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2022-03-09 14:28:51 -0300

    wip

---
+++ .vuepress/config-project.js
@@ -0,0 +1,52 @@
+module.exports = {
+    title: 'V4 Admin',
+    description: 'Chevereto V4 Admin Manual',
+    head: [
+        ['link', {
+            rel: 'icon',
+            href: `/logo.svg`
+        }],
+        ['link', {
+            rel: 'manifest',
+            href: '/manifest.json'
+        }],
+        ['meta', {
+            name: 'theme-color',
+            content: '#23a8e0'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-capable',
+            content: 'yes'
+        }],
+        ['meta', {
+            name: 'apple-mobile-web-app-status-bar-style',
+            content: 'black'
+        }],
+        ['link', {
+            rel: 'apple-touch-icon',
+            href: '/logo.svg'
+        }],
+        ['link', {
+            rel: 'mask-icon',
+            href: '/logo.svg',
+            color: '#3eaf7c'
+        }],
+        ['meta', {
+            name: 'msapplication-TileImage',
+            content: '/logo.svg'
+        }],
+        ['meta', {
+            name: 'msapplication-TileColor',
+            content: '#000000'
+        }]
+    ],
+    themeConfig: {
+        logo: '/logo.svg',
+        // repo: 'chevereto/chevereto',
+        docsRepo: 'chevereto/v4-admin',
+        docsBranch: 'main',
+        smoothScroll: true,
+        editLinks: true,
+        lastUpdated: true,
+    },
+};
--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# v4-admin
+---
+home: true
+heroImage: /logo.svg
+heroText: V4 Admin Manual
+tagline: Documentation for administrators of Chevereto V4 based systems
+# actionText: Dashboard →
+# actionLink: /dashboard/
+footer: Admin's Manual for Chevereto V4
+---
+
+# 👸 Admin Manual
+
+Welcome to the Chevereto V4 Admin Manual.
+
+💡 This documentation contains all what's need to know about administrating a Chevereto-based system.
+
+## User Manual
+
+Not an admin? You can check the [Chevereto V4 user's manual](https://v4-user.chevereto.com/).
+
+## Links
+
+* [Website](https://chevereto.com/)
+* [Community](https://chevereto.com/community/)
+* [Releases](https://releases.chevereto.com/)
