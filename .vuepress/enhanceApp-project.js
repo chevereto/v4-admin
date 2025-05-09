6d0f2b3d844777cda96ecbecf2989c6236e9cdc8
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2025-05-09 11:27:29 -0400

    typos

--- .vuepress/enhanceApp-project.js
+++ .vuepress/enhanceApp-project.js
@@ -1,10 +1,8 @@
 export default ({ router }) => {
     router.addRoutes([
       { path: '/features/external-storage.html', redirect: '/features/upload-storage/' },
-      { path: '/features/external-storage.html', redirect: '/features/upload-storage/' },
       { path: '/settings/asset-storage.html', redirect: '/settings/site-storage/' },
       { path: '/settings/external-storage.html', redirect: '/settings/upload-storage/' },
-      { path: '/features/external-storage/', redirect: '/features/upload-storage/' },
       { path: '/features/external-storage/', redirect: '/features/upload-storage/' },
       { path: '/settings/asset-storage/', redirect: '/settings/site-storage/' },
       { path: '/settings/external-storage/', redirect: '/settings/upload-storage/' },
